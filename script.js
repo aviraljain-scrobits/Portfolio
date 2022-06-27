607944bb2cde7b4dade758a90fd143107ce473ef
Aviral Jain <aviraljn1512@gmail.com>
2022-06-28 01:33:25 +0530

    Personal Portfolio added

--- script.js
+++ script.js
@@ -2,14 +2,28 @@ $(document).ready(function(){
     $(window).scroll(function(){
         if(this.scrollY > 20){
             $('.navbar').addClass("sticky");
-        }else{
-            $('.navbar').removeClass("sticky");
         }
-});
-
-// toggle menu/navbar script
-$('.menu-btn').click(function(){
-    $('.navbar .menu').toggleClass("active");
-    $('.menu-btn i').toggleClass("active");
+        else{
+$('.navbar').removeClass("sticky");
+        }
     });
 });
+
+$('.menu-btn').click(function(){
+    $('.navbar .menu').toggleClass("active")
+    $('.menu-btn i').toggleClass("active")
+});
+
+// typing
+var typed=new Typed (".typing",{
+strings:["Cloud Enthusiast","Brand Manager","Developer","Designer","Content Writer"],
+typeSpeed: 100,
+backSpeed: 60,
+loop: true
+});
+var typed=new Typed (".typing-2",{
+    strings:["Cloud Enthusiast","Brand Manager","Developer","Designer","Content Writer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+    });
